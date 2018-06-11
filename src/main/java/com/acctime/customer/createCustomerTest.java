d8bb54b7d2e92be5fe6717fbe91acf67b808cf06
Puneeth <Puneeth@Puneeth-PC>
2018-06-11 12:51:18 +0530

    Q2W211

---
+++ src/main/java/com/acctime/customer/createCustomerTest.java
@@ -0,0 +1,23 @@
+package com.acctime.customer;
+
+import org.testng.annotations.Test;
+
+public class createCustomerTest {
+	
+	@Test
+	
+	public void createCustomerTest()
+	{
+		System.out.println("execute createCustomerTest");
+	}
+	@Test
+	public void modifyCustomerTest()
+	{
+		System.out.println("execute modifyCustomerTest");
+	}
+	@Test
+	public void deleteCustomerTest()
+	{
+		System.out.println("execute deleteCustomerTest");
+	}
+}
